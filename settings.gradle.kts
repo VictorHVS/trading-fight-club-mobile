0284aa2648ea98181bb3d3d586ad8195dd8d60dd
Victor Hugo <vhv.sousa@gmail.com>
2021-07-19 18:55:52 -0300

    hello world KMM!

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Trading_Fightclub"
+include(":androidTFC")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/victorhvs/tradingfightclub/Greeting.kt
@@ -0,0 +1,7 @@
+package com.victorhvs.tradingfightclub
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}! TFC Project"
+    }
+}
