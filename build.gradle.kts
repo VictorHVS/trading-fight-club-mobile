040d744a15106ebca72c6b194b7714508fc8277c
Victor Hugo Vieira de Sousa <vhv.sousa@gmail.com>
2022-11-26 19:34:52 -0300

    added renovate.json
    Signed-off-by: Victor Hugo Vieira de Sousa <vhv.sousa@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,3 @@
-//plugins {
-//    id "com.android.application" version "7.4.0-beta05" apply false
-//    id "com.android.library" version "7.4.0-beta05" apply false
-//    id "org.jetbrains.kotlin.android" version "1.7.10" apply false
-//}
-
 buildscript {
     repositories {
         google()
@@ -11,7 +5,7 @@ buildscript {
         maven(url = "https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("com.android.tools.build:gradle:7.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.10")
