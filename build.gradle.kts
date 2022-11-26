877043a51041566071e3b6482bc2f1523dd54a29
Victor Hugo <vhv.sousa@gmail.com>
2022-11-26 20:33:49 -0300

    Feature/foundation (#20)
    * updated: gradle to gradle.kts; material3 files; app icon
    added: libs.toml
    
    Signed-off-by: Victor Hugo Vieira de Sousa <vhv.sousa@gmail.com>
    
    * added renovate.json
    
    Signed-off-by: Victor Hugo Vieira de Sousa <vhv.sousa@gmail.com>
    
    * added CODEOWNERS and funding file
    
    Signed-off-by: Victor Hugo Vieira de Sousa <vhv.sousa@gmail.com>
    
    Signed-off-by: Victor Hugo Vieira de Sousa <vhv.sousa@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.10")
+        classpath("com.google.gms:google-services:4.3.14")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
+        classpath("org.jacoco:org.jacoco.core:0.8.8")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
