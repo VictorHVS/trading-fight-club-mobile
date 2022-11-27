4482ea325a8ed3926ee3425078cd66416dd9ff43
VictorHVS <vhv.sousa@gmail.com>
2022-11-27 11:42:59 -0300

    chore: add kover
    Signed-off-by: Victor Hugo Vieira de Sousa <vhv.sousa@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import kotlinx.kover.api.KoverProjectConfig
+
 buildscript {
     repositories {
         google()
@@ -12,6 +14,34 @@ buildscript {
         classpath("com.google.gms:google-services:4.3.14")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("org.jacoco:org.jacoco.core:0.8.8")
+    }
+}
+
+plugins {
+    id("org.jetbrains.kotlinx.kover") version "0.6.1"
+}
+
+allprojects {
+    apply(plugin = "kover")
+
+    group = "com.victorvs.tfc"
+    version = "0.1.0"
+
+    extensions.configure<KoverProjectConfig> {
+        isDisabled.set(false)
+        engine.set(kotlinx.kover.api.JacocoEngine("0.8.8"))
+    }
+
+    koverMerged {
+        enable()
+        xmlReport {
+            onCheck.set(false)
+            reportFile.set(layout.buildDirectory.file("$buildDir/reports/kover/result.xml"))
+        }
+        htmlReport {
+            onCheck.set(false)
+            reportDir.set(layout.buildDirectory.dir("$buildDir/reports/kover/html-result"))
+        }
     }
 }
 
